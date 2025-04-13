60c5f1fa3a33e32fc6dce076044d33411f9fa01d
jakereckin <111083093+jakereckin@users.noreply.github.com>
2025-04-13 11:14:33 -0500

    Update 04-Add_Shots.py

--- pages/04-Add_Shots.py
+++ pages/04-Add_Shots.py
@@ -20,7 +20,9 @@ db_clubs = pl.from_pandas(data=db_clubs)
 
 round_id = st.selectbox(
     label='Round ID',
-    options=db_rounds['ROUND_ID'].to_list()
+    options=db_rounds['ROUND_ID'].to_list(),
+    index=None,
+    placeholder='Select Round ID'
 )
 def get_round_info(db_rounds, round_id):
     """
@@ -89,152 +91,153 @@ this_round, db_holes = get_round_info(db_rounds=db_rounds, round_id=round_id)
 merged_round_info = merge_round_info(this_round=this_round, db_holes=db_holes)
 shots_in_round = get_shots_in_round(merged_round_info=merged_round_info)
 
-hole_add = st.selectbox(
-    label='Hole',
-    options=merged_round_info['HOLE'].to_list(),
-    placeholder='Select Hole',
-    index=None
-)
-if hole_add:
-    shots_in_round = get_shots_in_round(merged_round_info=merged_round_info)
-    merged_round_info = merged_round_info.with_columns(
-        pl.col(name='HOLE').cast(pl.Int32)
+if round_id:
+    hole_add = st.selectbox(
+        label='Hole',
+        options=merged_round_info['HOLE'].to_list(),
+        placeholder='Select Hole',
+        index=None
     )
-    hole_add = int(hole_add)
-    this_hole = merged_round_info.filter(
-        pl.col(name='HOLE') == hole_add
-    )
-    st.write(shots_in_round)
-    hole_par = this_hole['PAR'].to_list()[0]
-    hole_distance = this_hole['DISTANCE'].to_list()[0]
-    par, distance = st.columns(spec=2)
-    with par:
-        st.write(f'Hole {hole_add} Par: {hole_par}')
-    with distance:
-        st.write(f'Hole {hole_add} Distance: {hole_distance}')
-    shots_in_round = shots_in_round.with_columns(
-         pl.col(name='HOLE').cast(pl.Int32)
-    )
-    if len(shots_in_round['HOLE']) == 0:
-         data = {
-              'HOLE': '',
-              'SHOT_NUMBER': '',
-                'SHOT_TYPE': '',
-                'DISTANCE': '',
-                'CLUB': ''
-         }
-         last_shot = pl.DataFrame(data=[data])
-         max_shot = 1
-    else:      
-        hole_data = shots_in_round.filter(
-            pl.col(name='HOLE') == int(hole_add)
+    if hole_add:
+        shots_in_round = get_shots_in_round(merged_round_info=merged_round_info)
+        merged_round_info = merged_round_info.with_columns(
+            pl.col(name='HOLE').cast(pl.Int32)
         )
-        max_shot = hole_data['SHOT_NUMBER'].max()
-        if max_shot is None:
+        hole_add = int(hole_add)
+        this_hole = merged_round_info.filter(
+            pl.col(name='HOLE') == hole_add
+        )
+        st.write(shots_in_round)
+        hole_par = this_hole['PAR'].to_list()[0]
+        hole_distance = this_hole['DISTANCE'].to_list()[0]
+        par, distance = st.columns(spec=2)
+        with par:
+            st.write(f'Hole {hole_add} Par: {hole_par}')
+        with distance:
+            st.write(f'Hole {hole_add} Distance: {hole_distance}')
+        shots_in_round = shots_in_round.with_columns(
+            pl.col(name='HOLE').cast(pl.Int32)
+        )
+        if len(shots_in_round['HOLE']) == 0:
+            data = {
+                'HOLE': '',
+                'SHOT_NUMBER': '',
+                    'SHOT_TYPE': '',
+                    'DISTANCE': '',
+                    'CLUB': ''
+            }
+            last_shot = pl.DataFrame(data=[data])
             max_shot = 1
-        else:
-            max_shot += 1
-        last_shot = shots_in_round.filter(
-            pl.col(name='HOLE') == int(hole_add)
-        ).filter(
-            pl.col(name='SHOT_NUMBER') == max_shot - 1
-        )
-    st.dataframe(last_shot[['HOLE', 'SHOT_NUMBER', 'SHOT_TYPE', 'DISTANCE', 'CLUB']])
-    sht_type, number = st.columns(spec=2)
-    with sht_type:
-        shot_type = st.radio(
-            label='Shot Type',
-            options=['TEE', 'FAIRWAY', 'ROUGH', 'SAND', 'RECOVERY', 'GREEN'],
-            horizontal=True
-        )
-    with number:
-        shot_number = st.number_input(
-            label='Shot Number',
-            min_value=1,
-            max_value=20,
-            value=max_shot
+        else:      
+            hole_data = shots_in_round.filter(
+                pl.col(name='HOLE') == int(hole_add)
             )
-    if shot_number:
-        dist_entry, club_entry = st.columns(spec=2)
-        with dist_entry:
-            init_value = 0
-            if shot_number == 1:
-                init_value = hole_distance
-            distance = st.number_input(
-                    label='Distance',
-                    min_value=0,
-                    max_value=hole_distance + 100,
+            max_shot = hole_data['SHOT_NUMBER'].max()
+            if max_shot is None:
+                max_shot = 1
+            else:
+                max_shot += 1
+            last_shot = shots_in_round.filter(
+                pl.col(name='HOLE') == int(hole_add)
+            ).filter(
+                pl.col(name='SHOT_NUMBER') == max_shot - 1
             )
-        with club_entry:
-                club = st.radio(
-                    label='Club',
-                    options=db_clubs['CLUB_NAME'].to_list(),
-                    horizontal=True
+        st.dataframe(last_shot[['HOLE', 'SHOT_NUMBER', 'SHOT_TYPE', 'DISTANCE', 'CLUB']])
+        sht_type, number = st.columns(spec=2)
+        with sht_type:
+            shot_type = st.radio(
+                label='Shot Type',
+                options=['TEE', 'FAIRWAY', 'ROUGH', 'SAND', 'RECOVERY', 'GREEN'],
+                horizontal=True
+            )
+        with number:
+            shot_number = st.number_input(
+                label='Shot Number',
+                min_value=1,
+                max_value=20,
+                value=max_shot
                 )
-        miss_type, putt_type = st.columns(spec=2)
-        penalty_entry, make_entry = st.columns(spec=2)
-        with miss_type:
-                miss_shot_types = ['RIGHT', 'LEFT', 'LONG', 'SHORT', 'HIT']
-                miss_shot = st.radio(
-                    label='Missed Shot Type',
-                    options=miss_shot_types,
-                    horizontal=True
+        if shot_number:
+            dist_entry, club_entry = st.columns(spec=2)
+            with dist_entry:
+                init_value = 0
+                if shot_number == 1:
+                    init_value = hole_distance
+                distance = st.number_input(
+                        label='Distance',
+                        min_value=0,
+                        max_value=hole_distance + 100,
                 )
-        with putt_type:
-                putt_shot_types = [
-                    'U-LR', 'U-RL', 'U-S', 
-                    'D-LR', 'D-RL', 'D-S',
-                    'F-LR', 'F-RL', 'F-S'
-                ]
-                putt_shot = st.radio(
-                    label='Putt Shot Type',
-                    options=putt_shot_types,
-                    horizontal=True
-                )
-        with penalty_entry:
-                penalty_strokes = st.number_input(
-                    label='Penalty Strokes',
-                    min_value=0,
-                    max_value=2
-                )
-        with make_entry:
-                make = st.radio(
-                    label='Make',
-                    options=['No', 'Yes'],
-                    horizontal=True, 
+            with club_entry:
+                    club = st.radio(
+                        label='Club',
+                        options=db_clubs['CLUB_NAME'].to_list(),
+                        horizontal=True
+                    )
+            miss_type, putt_type = st.columns(spec=2)
+            penalty_entry, make_entry = st.columns(spec=2)
+            with miss_type:
+                    miss_shot_types = ['RIGHT', 'LEFT', 'LONG', 'SHORT', 'HIT']
+                    miss_shot = st.radio(
+                        label='Missed Shot Type',
+                        options=miss_shot_types,
+                        horizontal=True
+                    )
+            with putt_type:
+                    putt_shot_types = [
+                        'U-LR', 'U-RL', 'U-S', 
+                        'D-LR', 'D-RL', 'D-S',
+                        'F-LR', 'F-RL', 'F-S'
+                    ]
+                    putt_shot = st.radio(
+                        label='Putt Shot Type',
+                        options=putt_shot_types,
+                        horizontal=True
+                    )
+            with penalty_entry:
+                    penalty_strokes = st.number_input(
+                        label='Penalty Strokes',
+                        min_value=0,
+                        max_value=2
+                    )
+            with make_entry:
+                    make = st.radio(
+                        label='Make',
+                        options=['No', 'Yes'],
+                        horizontal=True, 
 
-                )
-        add = st.button(label='Add Shot')
-        if add:
-            with sqlitecloud.connect(sql_lite_connect) as conn:
-                if shot_type != 'GREEN':
-                    putt_type = 'N/A'
-                cursor = conn.cursor()
-                hole_add = str(hole_add)
-                shot_number = int(shot_number)
-                round_id = str(round_id)
-                shot_type = str(shot_type)
-                distance = int(distance)
-                penalty_strokes = int(penalty_strokes)
-                club = str(club)
-                miss_type = str(miss_shot)
-                putt_shot = str(putt_shot)
-                make = str(make)
-                cursor.execute(
-                        sql=sql.insert_shot_sql(),
-                        parameters=(
-                            round_id,
-                            hole_add,
-                            shot_number,
-                            shot_type,
-                            distance,
-                            club,
-                            penalty_strokes,
-                            miss_type,
-                            putt_shot,
-                            make
-                        )
-                )
-                conn.commit()
-            st.write('Shot Added')
-            st.rerun()
+                    )
+            add = st.button(label='Add Shot')
+            if add:
+                with sqlitecloud.connect(sql_lite_connect) as conn:
+                    if shot_type != 'GREEN':
+                        putt_type = 'N/A'
+                    cursor = conn.cursor()
+                    hole_add = str(hole_add)
+                    shot_number = int(shot_number)
+                    round_id = str(round_id)
+                    shot_type = str(shot_type)
+                    distance = int(distance)
+                    penalty_strokes = int(penalty_strokes)
+                    club = str(club)
+                    miss_type = str(miss_shot)
+                    putt_shot = str(putt_shot)
+                    make = str(make)
+                    cursor.execute(
+                            sql=sql.insert_shot_sql(),
+                            parameters=(
+                                round_id,
+                                hole_add,
+                                shot_number,
+                                shot_type,
+                                distance,
+                                club,
+                                penalty_strokes,
+                                miss_type,
+                                putt_shot,
+                                make
+                            )
+                    )
+                    conn.commit()
+                st.write('Shot Added')
+                st.rerun()
